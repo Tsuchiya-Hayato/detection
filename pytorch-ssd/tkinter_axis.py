980d35d64252cf79d404a885c1c25ae6ed2cbdbd
unknown <hayato14399@gmail.com>
2020-07-24 16:58:47 +0900

    UI改善

--- pytorch-ssd/tkinter_axis.py
+++ pytorch-ssd/tkinter_axis.py
@@ -29,33 +29,24 @@ class CovidApp(tk.Tk):
         self.wrpFrm.configure(bg="white")
         # 縦横ともに3pxの余白で、rootウィンドウいっぱいに配置
         self.wrpFrm.grid(row=0, column=0, sticky="nsew")
-        #########################
         
         ### 評価結果フレームの作成 ###
         self.evalFrm = tk.Frame(self.wrpFrm)
         self.evalFrm.configure(bg="white")
-        # 縦横ともに3pxの余白で、rootウィンドウいっぱいに配置
         self.evalFrm.pack(padx=100, pady=2, fill="both", expand=1)
-
-        # 評価結果テキストを配置
-        # ウィジェット生成
+        # 検出人数を表示
         self.person_count = tkk.Label(self.evalFrm)
         self.person_count.configure(text="検出人数：",font=("",30),foreground="black", background="white")
-        self.person_count.grid(row=0, column=0, sticky="nsew")
-
-        # ウィジェット生成
+        self.person_count.grid(padx=100,row=0, column=0, sticky="nsew")
+        #密集度を算出
         self.eval_density = tkk.Label(self.evalFrm)
         self.eval_density.configure(text="密集度　：",font=("",30),foreground="black", background="white")
-        self.eval_density.grid(row=1, column=0, sticky="nsew")
-
-        
+        self.eval_density.grid(padx=100,row=1, column=0, sticky="nsew")
 
         ### 動画表示フレームの作成 ###
         self.movieFrm = tk.Frame(self.wrpFrm)
         self.movieFrm.configure(bg="white")
-        # 縦横ともに3pxの余白で、rootウィンドウいっぱいに配置
         self.movieFrm.pack(padx=100, pady=2, fill="both", expand=1)
-
         # 動画表示キャンパスの配置
         self.movie_canvas = tk.Canvas(self.movieFrm, width = self.cap.width, height = self.cap.height)
         self.movie_canvas.pack()
@@ -66,26 +57,25 @@ class CovidApp(tk.Tk):
         self.threshhold.configure(bg="white")
         # 縦横ともに3pxの余白で、rootウィンドウいっぱいに配置
         self.threshhold.pack(padx=100, pady=1, fill="both", expand=1)
-
         # スライダーの作成 #
         self.eval_density = tkk.Label(self.threshhold)
         self.eval_density.configure(text="密集閾値",font=("",25),foreground="black", background="white")
         self.eval_density.grid(row=0, column=0, sticky="nsew")
         self.var_distance = tk.IntVar(master=self.threshhold,value=3,)
-        self.scale_distace = tk.Scale(master=self.threshhold, orient="h",variable=self.var_distance,from_=1, to=10,length=350)
+        self.scale_distace = tk.Scale(master=self.threshhold, orient="h",variable=self.var_distance,from_=1, to=10,length=250)
         self.scale_distace.grid(row=1, column=0, sticky="nsew")
 
         # モデル推論の閾値スライダー #
         self.eval_model = tkk.Label(self.threshhold) 
         self.eval_model.configure(text="モデル判定閾値",font=("",25),foreground="black", background="white")
-        self.eval_model.grid(row=0, column=1, sticky="nsew")
+        self.eval_model.grid(row=0, column=2, sticky="nsew",padx=40)
         self.var_model = tk.DoubleVar(master=self.threshhold,value=0.5,)
-        self.scale_model = tk.Scale(master=self.threshhold, orient="h",variable=self.var_model,from_=0, to=1,length=350, resolution=0.1)
-        self.scale_model.grid(row=1, column=1, sticky="nsew")
+        self.scale_model = tk.Scale(master=self.threshhold, orient="h",variable=self.var_model,from_=0, to=1,length=250, resolution=0.1)
+        self.scale_model.grid(row=1, column=2, sticky="nsew",padx=40)
 
 
         #--------------------------------------------------------------------------
-        ### 初期設定frame ###
+        ### 初期設定frame1 ###
         self.frame1 = tk.Frame(self)
         self.frame1.configure(bg="white")
         self.frame1.grid(row=0, column=0, sticky="nsew")
@@ -101,7 +91,6 @@ class CovidApp(tk.Tk):
         self.ip_entry.insert(0, "192.168.100")
         self.ip_entry.pack(ipadx=25,ipady=10,pady=3, expand=1)
 
-
         ### music入力
         self.music_frame = tk.Frame(self.frame1)
         self.music_frame.configure(bg="white")
@@ -116,52 +105,43 @@ class CovidApp(tk.Tk):
         self.button1 = tk.Button(self.music_frame, text='ファイル選択', command=self.button1_clicked, font=("", 15))
         self.button1.pack(pady=1, expand=1)
         # 参照ファイルパス表示ラベルの作成
-        self.file1 = tk.StringVar()
-        self.file1_entry = tkk.Entry(self.music_frame, textvariable=self.file1, width=40, font=("",15))
+        self.music_file = tk.StringVar()
+        self.file1_entry = tkk.Entry(self.music_frame, textvariable=self.music_file, width=40, font=("",15))
         self.file1_entry.pack(ipady=10, pady=3, expand=1)
         
-
         ### フレーム1からmainフレームに戻るボタン
-         ### music入力
         self.button_frame = tk.Frame(self.frame1)
         self.button_frame.configure(bg="white")
         self.button_frame.pack(padx=100, pady=1, fill="both", expand=1)
         self.back_button = tk.Button(self.button_frame,  text="検査開始", command=lambda:self.changePage(self.wrpFrm), font=("", '15'))
         self.back_button.pack(ipadx=25,ipady=10,pady=1, expand=1)
-        #main_frameを一番上に表示
+
+        ### main_frameを一番上に表示
         self.frame1.tkraise()
-        #--------------------------------------------------------------------------
 
     def button1_clicked(self):
         fTyp = [("","*")]
         iDir = os.path.abspath(os.path.dirname(__file__))
         self.filepath = filedialog.askopenfilename(filetypes = fTyp,initialdir = iDir)
-        self.file1.set(self.filepath)
+        self.music_file.set(self.filepath)
 
     def changePage(self, page):
         #入力値を得る
         self.ip_adress = self.ip_entry.get()
-        print("カメラのIPアドレス",str(self.ip_adress))
-        print("音ファイル",str(self.filepath))
-        '''
-        画面遷移用の関数
-        '''
+        self.music_adress = self.music_file.get()
+        #画面遷移用の関数
         page.tkraise()
         # 更新作業
         self.update()
-        #########################
+
     def update(self):
-        '''
-        各キャンバスへの画像書き込み(opencvのimshow()的な処理)
-        '''
-        print(self.scale_distace.get())
-        print(self.scale_model.get())
-        # VideoCaptureクラスのget_frameで画像を取得
+        #画面更新
+        distance_scale = self.scale_distace.get()
+        thresh_scale = self.scale_model.get()
         try:
             ret, frame = self.cap.get_frame()
-            frame,cnt = self.detection.eval_frame(frame,self.scale_distace.get(),self.scale_model.get())
+            frame,cnt = self.detection.eval_frame(frame,distance_scale,thresh_scale)
             self.person_count.configure(text=("検出人数："+str(cnt)+'人'),font=("",30),foreground="black", background="white")
-        # 取得できなかったら
         except:
             print('update失敗')
             ret = False
@@ -220,21 +200,21 @@ class Detection():
         print('Detection_init読み込み終了')
 
 
-    def eval_frame(self,img,distance,threshold):
-        print(distance)
+    def eval_frame(self,img,distance,threshold): #(画像、box間の距離閾値、モデル閾値)
         boxes, labels, probs = self.predictor.predict(img,10,threshold)
-        pos_box = []  #bboxの中心の座標を格納するlist
         for i in range(boxes.size(0)):
-            box = boxes[i, :]
-            pos_box.append([int((box[0]+box[2])/2), int((box[1]+box[3])/2)])
-        #各bboxの中心同士の長さを計算
-        #規定値より小さければ、線で結ぶ
-        for box_1 in pos_box:
-            for box_2 in pos_box:
-                box_distance  = (((box_1[0]-box_2[0])**2)+(box_1[1]-box_2[1])**2) ** 0.5
-                if box_distance < distance:
+            box_1 = boxes[i, :]
+            box_1x = int((box_1[0]+box_1[2])/2)
+            box_1y = int((box_1[1]+box_1[3])/2)
+            for j in range(boxes.size(0)):
+                box_2 = boxes[j, :]
+                box_2x = int((box_2[0]+box_2[2])/2)
+                box_2y = int((box_2[1]+box_2[3])/2)
+                box_distance  = (((box_1x - box_2x)**2)+(box_1y-box_2y)**2) ** 0.5
+
+                if (i == j) & (box_distance < distance):
                     cv2.line(img, (box_1[0],box_1[1]),(box_2[0],box_2[1]),(0,255,0),1)
-                    cv2.rectangle(img, (box_1[0], box_1[1]), (box_1[2], box_1[3]), (0, 255, 0), 4) 
+                    cv2.rectangle(img, (box_1[0], box_1[1]), (box_1[2], box_1[3]), (0, 255, 0), 4)
                 else:
                     cv2.rectangle(img, (box_1[0], box_1[1]), (box_1[2], box_1[3]), (255, 0, 0), 4)
 
