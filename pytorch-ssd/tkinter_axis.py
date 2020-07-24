7e714ee3277ea6a2f9312e1c8aa22f45e26833a4
unknown <hayato14399@gmail.com>
2020-07-24 16:17:15 +0900

    UI改善

--- pytorch-ssd/tkinter_axis.py
+++ pytorch-ssd/tkinter_axis.py
@@ -35,44 +35,53 @@ class CovidApp(tk.Tk):
         self.evalFrm = tk.Frame(self.wrpFrm)
         self.evalFrm.configure(bg="white")
         # 縦横ともに3pxの余白で、rootウィンドウいっぱいに配置
-        self.evalFrm.pack(padx=100, pady=3, fill="both", expand=1)
+        self.evalFrm.pack(padx=100, pady=2, fill="both", expand=1)
 
         # 評価結果テキストを配置
-        ## ウィジェット生成
+        # ウィジェット生成
         self.person_count = tkk.Label(self.evalFrm)
         self.person_count.configure(text="検出人数：",font=("",30),foreground="black", background="white")
         self.person_count.grid(row=0, column=0, sticky="nsew")
 
-        ## ウィジェット生成
+        # ウィジェット生成
         self.eval_density = tkk.Label(self.evalFrm)
         self.eval_density.configure(text="密集度　：",font=("",30),foreground="black", background="white")
         self.eval_density.grid(row=1, column=0, sticky="nsew")
 
-        ## スライダーの作成
-        self.eval_density = tkk.Label(self.evalFrm)
-        self.eval_density.configure(text="密集閾値",font=("",30),foreground="black", background="white")
-        self.eval_density.grid(row=2, column=0, sticky="nsew")
-        self.var_distance = tk.IntVar(master=self.evalFrm,value=3,)
-        self.scale_distace = tk.Scale(master=self.evalFrm, orient="h",variable=self.var_distance,from_=1, to=10,length=350)
-        self.scale_distace.grid(row=2, column=1, sticky="nsew")
-
-        ## モデル推論の閾値スライダー
-        self.eval_model = tkk.Label(self.evalFrm) 
-        self.eval_model.configure(text="モデル判定閾値",font=("",30),foreground="black", background="white")
-        self.eval_model.grid(row=3, column=0, sticky="nsew")
-        self.var_model = tk.DoubleVar(master=self.evalFrm,value=0.5,)
-        self.scale_model = tk.Scale(master=self.evalFrm, orient="h",variable=self.var_model,from_=0, to=1,length=350, resolution=0.1)
-        self.scale_model.grid(row=3, column=1, sticky="nsew")
+        
 
         ### 動画表示フレームの作成 ###
         self.movieFrm = tk.Frame(self.wrpFrm)
         self.movieFrm.configure(bg="white")
         # 縦横ともに3pxの余白で、rootウィンドウいっぱいに配置
-        self.movieFrm.pack(padx=100, pady=3, fill="both", expand=1)
+        self.movieFrm.pack(padx=100, pady=2, fill="both", expand=1)
 
         # 動画表示キャンパスの配置
         self.movie_canvas = tk.Canvas(self.movieFrm, width = self.cap.width, height = self.cap.height)
         self.movie_canvas.pack()
+
+
+        ### 閾値調整フレームの作成 ###
+        self.threshhold = tk.Frame(self.wrpFrm)
+        self.threshhold.configure(bg="white")
+        # 縦横ともに3pxの余白で、rootウィンドウいっぱいに配置
+        self.threshhold.pack(padx=100, pady=1, fill="both", expand=1)
+
+        # スライダーの作成 #
+        self.eval_density = tkk.Label(self.threshhold)
+        self.eval_density.configure(text="密集閾値",font=("",25),foreground="black", background="white")
+        self.eval_density.grid(row=0, column=0, sticky="nsew")
+        self.var_distance = tk.IntVar(master=self.threshhold,value=3,)
+        self.scale_distace = tk.Scale(master=self.threshhold, orient="h",variable=self.var_distance,from_=1, to=10,length=350)
+        self.scale_distace.grid(row=1, column=0, sticky="nsew")
+
+        # モデル推論の閾値スライダー #
+        self.eval_model = tkk.Label(self.threshhold) 
+        self.eval_model.configure(text="モデル判定閾値",font=("",25),foreground="black", background="white")
+        self.eval_model.grid(row=0, column=1, sticky="nsew")
+        self.var_model = tk.DoubleVar(master=self.threshhold,value=0.5,)
+        self.scale_model = tk.Scale(master=self.threshhold, orient="h",variable=self.var_model,from_=0, to=1,length=350, resolution=0.1)
+        self.scale_model.grid(row=1, column=1, sticky="nsew")
 
 
         #--------------------------------------------------------------------------
@@ -82,30 +91,43 @@ class CovidApp(tk.Tk):
         self.frame1.grid(row=0, column=0, sticky="nsew")
 
         ### カメラのIP入力
-        self.ip_camera = tkk.Label(self.frame1)
+        self.camera_frame = tk.Frame(self.frame1)
+        self.camera_frame.configure(bg="white")
+        self.camera_frame.pack(padx=100, pady=10, expand=1)
+        self.ip_camera = tkk.Label(self.camera_frame)
         self.ip_camera.configure(text="▽カメラのIPアドレスを入力▽",font=("",30),foreground="black", background="white")
-        self.ip_camera.place(x=180, y=10)
-        self.ip_entry = tkk.Entry(self.frame1)
-        self.ip_entry.place(x=300, y=80,width=200,height=30)
+        self.ip_camera.pack(pady=1, expand=1)
+        self.ip_entry = tkk.Entry(self.camera_frame, font=("",20))
+        self.ip_entry.insert(0, "192.168.100")
+        self.ip_entry.pack(ipadx=25,ipady=10,pady=3, expand=1)
 
-        ### 音ファイル選択
+
+        ### music入力
+        self.music_frame = tk.Frame(self.frame1)
+        self.music_frame.configure(bg="white")
+        self.music_frame.pack(padx=100, pady=1, fill="both", expand=1)
+        # 音ファイル選択
         self.s = tk.StringVar()
-        self.s.set('音声ファイル')
-        self.label1 = tkk.Label(self.frame1)
+        self.s.set('音声ファイルを指定')
+        self.label1 = tkk.Label(self.music_frame)
         self.label1.configure(textvariable=self.s, font=("", '30'),foreground="black", background="white")
-        self.label1.place(x=240, y=150)
-
+        self.label1.pack(pady=3, expand=1)
+        # 参照ボタンの作成
+        self.button1 = tk.Button(self.music_frame, text='ファイル選択', command=self.button1_clicked, font=("", 15))
+        self.button1.pack(pady=1, expand=1)
         # 参照ファイルパス表示ラベルの作成
         self.file1 = tk.StringVar()
-        self.file1_entry = tkk.Entry(self.frame1, textvariable=self.file1, width=50)
-        self.file1_entry.place(x=350, y=200)
-        # 参照ボタンの作成
-        self.button1 = tk.Button(self.frame1, text='参照', command=self.button1_clicked, font=("", '15'))
-        self.button1.place(x=500, y=150)
+        self.file1_entry = tkk.Entry(self.music_frame, textvariable=self.file1, width=40, font=("",15))
+        self.file1_entry.pack(ipady=10, pady=3, expand=1)
+        
 
-        # フレーム1からmainフレームに戻るボタン
-        self.back_button = tk.Button(self.frame1,  text="検査開始", command=lambda:self.changePage(self.wrpFrm), font=("", '15'))
-        self.back_button.place(x=300, y=300)
+        ### フレーム1からmainフレームに戻るボタン
+         ### music入力
+        self.button_frame = tk.Frame(self.frame1)
+        self.button_frame.configure(bg="white")
+        self.button_frame.pack(padx=100, pady=1, fill="both", expand=1)
+        self.back_button = tk.Button(self.button_frame,  text="検査開始", command=lambda:self.changePage(self.wrpFrm), font=("", '15'))
+        self.back_button.pack(ipadx=25,ipady=10,pady=1, expand=1)
         #main_frameを一番上に表示
         self.frame1.tkraise()
         #--------------------------------------------------------------------------
