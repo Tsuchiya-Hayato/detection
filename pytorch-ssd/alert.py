c3731d8556a83ea9be48ff475de631fba96f844f
unknown <hayato14399@gmail.com>
2020-08-05 00:15:25 +0900

    音の重複回避

--- pytorch-ssd/alert.py
+++ pytorch-ssd/alert.py
@@ -2,7 +2,13 @@ from playsound import playsound
 import sys
 
 path = sys.argv
+flag = 1 #最初のサウンド
 print("music start!!")
 print(path[1])
-playsound(path[1])
+if flag == 0:
+    print('音声中')
+elif flag == 1:
+    flag = 0 #サウンド中は繰り返し鳴らさない
+    playsound(path[1])
+    flag = 1 #鳴り終わったらサウンド可能に
 print("music end!!")
